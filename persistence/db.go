db8563e4831a82d10d168a4eb1e6896501d0f305
Ben Reichelt <ben.reichelt@gmail.com>
2015-11-11 21:14:23 -0600

    Moving db credentials to the config file

--- persistence/db.go
+++ persistence/db.go
@@ -2,12 +2,16 @@ package persistence
 
 import (
 	"database/sql"
+	"github.com/benreic/dingus/config"
 	_ "github.com/go-sql-driver/mysql"
 )
 
 func NewDb() *sql.DB {
+
+	credentials := config.DbUsername() + ":" + config.DbPassword()
+
 	db, err := sql.Open("mysql",
-		"dingus_user:secret@tcp(127.0.0.1:3306)/dingus")
+		credentials+"@tcp(127.0.0.1:3306)/dingus")
 
 	if err != nil {
 		panic(err)
